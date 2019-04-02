6f57762107ab79adc63b49d613b7100194a54362
Jean-Sébastien BERON <jean-sebastien.beron@itelios.com>
2019-04-02 14:14:25 +0200

    feat(TASK-2): GLOBAL - first page routing

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var jsonData = {
+	    title: 'Elvorfia',
+        layout: 'home/pt_home.hbs'
+    };
+    res.render('home/home.hbs', jsonData);
 });
 
 module.exports = router;
